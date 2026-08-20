e9ca4539e29f7d065dc20e3bb249ff59ac65a713
Tanay Das <dastanay286@gmail.com>
2026-08-20 23:10:27 +0530

    customer search route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+
 import userRoutes from './users.js';
+import customerRoutes from './customer.js';
 
 const router = Router();
 
 
 router.use('/users', userRoutes);
+router.use('/customers', customerRoutes);
 
 export default router;
